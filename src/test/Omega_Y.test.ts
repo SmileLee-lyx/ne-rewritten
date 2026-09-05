d4cfe7dc59491c425132c9961a2ea55b35a7cf91
Smile Lee <smileleelyx@qq.com>
2026-09-05 17:18:26 +0800

    修复错误测试样例

--- src/test/Omega_Y.test.ts
+++ src/test/Omega_Y.test.ts
@@ -3,9 +3,9 @@ import { omega_Y_actual, omega_Y_medium, omega_Y_strong, omega_Y_weak } from '@/
 
 describe('expand', () => {
     it('1343', () => {
-        expect(omega_Y_weak.FS([1, 3, 4, 3], 2)).toEqual([1, 3, 4, 2, 5, 9, 4, 9, 18, 8]);
-        expect(omega_Y_medium.FS([1, 3, 4, 3], 2)).toEqual([1, 3, 4, 2, 5, 9, 11, 16, 25, 36]);
-        expect(omega_Y_actual.FS([1, 3, 4, 3], 2)).toEqual([1, 3, 4, 2, 5, 9, 11, 16, 25, 36]);
-        expect(omega_Y_strong.FS([1, 3, 4, 3], 2)).toEqual([1, 3, 4, 2, 5, 9, 11, 16, 25, 36]);
+        expect(omega_Y_weak.FS([1, 3, 4, 3], 2)).toEqual([1, 3, 4, 2, 5, 9, 4, 9, 18]);
+        expect(omega_Y_medium.FS([1, 3, 4, 3], 2)).toEqual([1, 3, 4, 2, 5, 9, 11, 16, 25]);
+        expect(omega_Y_actual.FS([1, 3, 4, 3], 2)).toEqual([1, 3, 4, 2, 5, 9, 11, 16, 25]);
+        expect(omega_Y_strong.FS([1, 3, 4, 3], 2)).toEqual([1, 3, 4, 2, 5, 9, 11, 16, 25]);
     });
 });
